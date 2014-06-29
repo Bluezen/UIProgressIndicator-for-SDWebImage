9d6cd80fde05da889687853d23e45ff31fc6b934
Bluezen <nankuranaisai@gmail.com>
2014-06-30 01:50:39 +0200

    Replace IDMPhoto with MHVideoPhotoGallery & Add Video section in menu

--- UIImageView+UIProgressIndicatorForSDWebImage.h
+++ UIImageView+UIProgressIndicatorForSDWebImage.h
@@ -12,7 +12,7 @@
 #import <SDWebImage/UIImageView+WebCache.h>
 #import <SDWebImage/SDImageCache.h>
 
-#import "DACircularProgressView.h"
+#import <DACircularProgress/DACircularProgressView.h>
 
 @interface UIImageView (DACircularProgressIndicatorForSDWebImage)
 
